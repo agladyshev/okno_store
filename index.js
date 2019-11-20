38756c45b15a9b023344e8fb9f1cca5432387dc3
Alexey Gladyshev <aglad@pm.me>
2019-11-20 12:44:48 +0300

    dev: Allow any origin

--- index.js
+++ index.js
@@ -6,25 +6,34 @@ const port = 3000;
 require('dotenv').config();
 
 let baseURL = `https://${process.env.INSALES_KEY}:${process.env.INSALES_PASSWORD}@${process.env.INSALES_HOSTNAME}`
-    
+
 app.use('/', express.static('public'));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+    next();
+});
 
 var getCollections = function (req, res, next) {
     fetch(new URL('/admin/collections.json', baseURL))
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(error => console.log(error));
+        .then(res => res.json())
+        .then((json) => {
+            res.collections = json;
+            next();
+        })
+        .catch(error => console.log(error));
 }
 
 var getProducts = function (req, res, next) {
     fetch(new URL('/admin/products.json', baseURL))
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(error => console.log(error));
+        .then(res => res.json())
+        .then(json => console.log(json))
+        .catch(error => console.log(error));
 }
 
 app.get('/getCollections', getCollections, function (req, res, next) {
-    res.send(res.categories);
+    res.send(res.collections);
 });
 
 app.listen(port, () => console.log(`Listening`)); 
