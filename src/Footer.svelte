7e7d79f5f0956add22122dbf33d90e343394b29b
Alexey Gladyshev <aglad@pm.me>
2019-12-01 14:28:53 +0300

    fix: use png as phone icon

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -8,19 +8,20 @@
   }
   footer a {
     text-align: center;
-    /* padding: 0.25rem 0; */
     flex: 1;
     margin: 0;
-    font-size: 1.2rem;
+    text-decoration: none;
   }
   footer a img {
-    /* margin-top: 0.05rem; */
     height: 1.5rem;
+    width: 1.5rem;
   }
 </style>
 
 <footer>
-  <a href="tel:+79774879349">&#128222;</a>
+  <a href="tel:+79774879349">
+    <img src="/phone.png" alt="phone icon" />
+  </a>
   <a
     href="https://www.google.com/maps/place/%D0%9E%D0%BB%D1%8C%D1%85%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D1%83%D0%BB.,+14%D0%BA4,+%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0,+105006/@55.77476,37.6694118,19z/data=!3m1!4b1!4m5!3m4!1s0x46b54a8004cec531:0x134da9c3282cb595!8m2!3d55.77476!4d37.669959">
     <img src="/google-maps.png" alt="google maps icon" />
