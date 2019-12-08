3cb59b632acb9240fe73925ee2ab0a422135f387
Alexey Gladyshev <aglad@pm.me>
2019-12-08 18:14:45 +0300

    refactor: use CORS module

--- index.js
+++ index.js
@@ -1,51 +1,13 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import cors from 'cors'
 import { getCollections, getProducts, checkAvailability, addOrder, getPayment, getDelivery, getPromo } from './middleware.js'
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000;
 
-// const cors = require("cors");
-
-// const whitelist = [
-//   "http://winxdow.ru",
-//   "http://okno.com.ru",
-//   "http://okno.store",
-//   "http://okno.herokuapp.com",
-//   "https://winxdow.ru",
-//   "https://okno.com.ru",
-//   "https://okno.store",
-//   "https://okno.herokuapp.com",
-//   "http://localhost:5000"
-// ];
-
-// const corsOptions = {
-//   origin: function(origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   }
-// };
-
-
-// app.use(cors(corsOptions));
-
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://okno.herokuapp.com/");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
-//  next();
-// });
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
-  next();
-});
-
+app.use(cors())
 
 app.use("/", express.static("public"));
 
