150fddbdeea03dd1f0aa7fbc82bca1cc6fe81a6a
Alexey Gladyshev <aglad@pm.me>
2020-04-01 22:38:40 +0300

    fix: Instagram link

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -26,7 +26,7 @@
     href="https://www.google.com/maps/place/%D0%9E%D0%BB%D1%8C%D1%85%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D1%83%D0%BB.,+14%D0%BA4,+%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0,+105006/@55.77476,37.6694118,19z/data=!3m1!4b1!4m5!3m4!1s0x46b54a8004cec531:0x134da9c3282cb595!8m2!3d55.77476!4d37.669959">
     <img src="/google-maps.png" alt="google maps icon" />
   </a>
-  <a href="https://www.instagram.com/winxdow/">
+  <a href="https://www.instagram.com/oknostore/">
     <img src="/instagram.png" alt="instagram icon" />
   </a>
   <a href="https://www.facebook.com/winxdowru/">
